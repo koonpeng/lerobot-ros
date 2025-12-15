f8188c532871e043c45f32c508562d8bd2a71fe9
Teo Koon Peng <teokoonpeng@gmail.com>
2025-12-15 11:25:56 +0800

    add support for ros2 cameras
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- lerobot_robot_ros/lerobot_robot_ros/__init__.py
+++ lerobot_robot_ros/lerobot_robot_ros/__init__.py
@@ -1,2 +1,3 @@
+from .camera import ROS2Camera, ROS2CameraConfig
 from .config import AnninAR4Config, ROS2Config, SO101ROSConfig
 from .robot import SO101ROS, AnninAR4, ROS2Robot
---
+++ lerobot_robot_ros/lerobot_robot_ros/camera.py
@@ -0,0 +1,92 @@
+import threading
+import time
+from dataclasses import dataclass
+from typing import Any
+
+import numpy as np
+import rclpy
+import rclpy.qos
+from lerobot.cameras import Camera, CameraConfig, ColorMode
+from numpy.typing import NDArray
+from rclpy.executors import SingleThreadedExecutor
+from rclpy.node import Node
+from rclpy.subscription import Subscription
+from sensor_msgs.msg import Image
+
+
+@CameraConfig.register_subclass("ros2")
+@dataclass
+class ROS2CameraConfig(CameraConfig):
+    topic: str
+
+
+class ROS2Camera(Camera):
+    executor: SingleThreadedExecutor | None = None
+    executor_thread: threading.Thread | None = None
+
+    def __init__(self, config: ROS2CameraConfig) -> bool:
+        super().__init__(config)
+        self.config = config
+        self.node: Node | None = None
+        self.image_sub: Subscription[Image] | None = None
+        self.last_image: Image | None = None
+
+    @property
+    def is_connected(self) -> bool:
+        return self.image_sub is not None
+
+    @staticmethod
+    def find_cameras() -> list[dict[str, Any]]:
+        if not rclpy.ok():
+            rclpy.init()
+        node = Node("lerobot_ros_find_cameras")
+        time.sleep(2)  # wait for discovery
+        topics = node.get_topic_names_and_types()
+        camera_topics = [t for t in topics if t[1][0] == "sensor_msgs/msg/Image"]
+        return [{"topic": t[0]} for t in camera_topics]
+
+    def connect(self, warmup: bool = True) -> None:
+        if not rclpy.ok():
+            rclpy.init()
+        if self.executor is None:
+            self.executor = SingleThreadedExecutor()
+            self.executor_thread = threading.Thread(
+                target=self.executor.spin, daemon=True
+            )
+            self.executor_thread.start()
+
+        self.node = Node("lerobot_ros_camera")
+
+        def on_recv(msg: Image):
+            self.last_image = msg
+
+        self.image_sub = self.node.create_subscription(
+            Image,
+            self.config.topic,
+            on_recv,
+            rclpy.qos.qos_profile_sensor_data,
+        )
+
+        self.executor.add_node(self.node)
+        self.executor.wake()
+
+    def read(self, color_mode: ColorMode | None = None) -> NDArray:
+        if not self.last_image:
+            return np.array([], dtype=np.uint8)
+        if color_mode is not None and not self.last_image.encoding.startswith(
+            color_mode
+        ):
+            raise NotImplementedError("image conversion not supported yet")
+        return np.array(self.last_image.data, dtype=np.uint8).reshape(
+            (self.config.width, self.config.height, 3)
+        )
+
+    def async_read(self, timeout_ms: float = 0) -> NDArray:
+        return self.read()
+
+    def disconnect(self) -> None:
+        self.executor.remove_node(self.node)
+        self.image_sub.destroy()
+        self.image_sub = None
+        self.node.destroy_node()
+        self.node = None
